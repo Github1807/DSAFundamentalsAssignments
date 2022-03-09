7131332f2dd355764755b3377e3e5ae51e6ba51e
Github1807 <94870501+Github1807@users.noreply.github.com>
2022-03-09 15:22:49 +0530

    Q2 March 7th, 2k22

---
+++ RecursiveCodeForFibonacciSeries.java
@@ -0,0 +1,21 @@
+package assignments;
+
+public class RecursiveCodeForFibonacciSeries {
+    public static int fibonacci(int a){
+        int result=0;
+        if(a==0 || a==1){
+            result=1;
+        }
+        else{
+          result=fibonacci(a-1)+fibonacci(a-2);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int a=10;
+        for(int i=0;i<a;i++){
+            System.out.print(fibonacci(i)+" ,");
+        }
+
+    }
+}
