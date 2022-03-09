65421434dd86246f351ecb365dfa795988977fcb
Github1807 <94870501+Github1807@users.noreply.github.com>
2022-03-09 15:05:40 +0530

    Q1 March 7th, 2k22

---
+++ RecursiveCodeToFindMinMax.java
@@ -0,0 +1,26 @@
+package Assignments;
+
+public class RecursiveCodeToFindMinMax {
+    public static void FindMinMax(int arr[],int a,int min,int max){
+        if(a==arr.length){
+            System.out.println(min);
+            System.out.println(max);
+            return;
+        }
+        if(arr[a]<min){
+            min=arr[a];
+        }
+        if(arr[a]>max){
+            max=arr[a];
+        }
+        n++;
+       FindMinMax(arr,a,min,max);
+
+    }
+    public static void main(String[] args) {
+        int arr[]={1,-1,0,2,-2,3,-3,4,-4};
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        FindMinMax(arr,0,min, max);
+    }
+}
