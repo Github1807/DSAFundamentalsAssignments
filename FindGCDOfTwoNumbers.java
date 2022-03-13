f0d9758aaebdf9381b67f950173acd5f1b96bba2
Github1807 <94870501+Github1807@users.noreply.github.com>
2022-03-13 19:11:42 +0530

    Q4 March9th, 2k22

---
+++ FindGCDOfTwoNumbers.java
@@ -0,0 +1,18 @@
+package assignments;
+
+public class FindGCDOfTwoNumbers
+{
+    public static void gcd(int divisor,int divident){
+        if(divident%divisor==0){
+            System.out.println(divisor);
+            return;
+        }
+        gcd(divident%divisor,divisor);
+       // return divisor;
+    }
+    public static void main(String[] args) {
+        int a=4;
+        int b=8;
+        gcd(a,b);
+    }
+}
